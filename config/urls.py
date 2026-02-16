41046b5caa285bcb731443352162f8dd00590cdf
Thireb <muhammadahmadashiq@gmail.com>
2026-02-16 15:39:13 +0500

    adding redirect for default endpoint

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 """
 URL configuration for OSPOS project.
 """
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pos/', include('pos.urls')),
     path('inventory/', include('inventory.urls')),
+    path('', RedirectView.as_view(url='/pos/')),
 ]
