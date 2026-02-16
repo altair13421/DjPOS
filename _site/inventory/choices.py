136922d24d933bdab8cde8de2f8dfaf7fc47d8ea
Thireb <muhammadahmadashiq@gmail.com>
2026-02-16 21:18:01 +0500

    deleting unrequired folder

--- _site/inventory/choices.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-class StockChangeReason(models.TextChoices):
-    SALE = "SALE", _("Sale")
-    RESTOCK = "RESTOCK", _("Restock")
-    WASTE = "WASTE", _("Waste")
-    RETURN = "RETURN", _("Return")
